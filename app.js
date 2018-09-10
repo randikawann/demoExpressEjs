262284ecd28b83e59e8549021d61f15937462fe7
randikawann <randikawann@gmail.com>
2018-09-10 13:41:03 -0500

    added more params view ejs

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-// install template engine...  choosing ejs.
-// install ejs
 var express = require('express');
 var app = express();
 
-// set template engine as ejs
 app.set('view engine','ejs');
-// addedfolder views    -> profile.ejs
-// edit profile.ejs
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
@@ -15,11 +10,7 @@ app.get('/contact',function(req,res){
     res.sendFile(__dirname+'/contact.html');
 });
 app.get('/profile/:id',function(req,res){
-    // edit this
-            // res.send('loggin profile with user id :'+req.params.id);
-    // res.render('profile');
-            // edit this to add params... create object
-    res.render('profile',{person: req.params.id});
-    // added this to profile.ejs file
+    var data  = {name:'randika', age : '24'};
+    res.render('profile',{person: req.params.id, data : data});
 });
 app.listen(3000);
