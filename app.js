0ebdbbe85a17e0ef5dcc00fa5192830b523d1c8d
randikawann <randikawann@gmail.com>
2018-09-10 11:23:27 -0500

    added basic routing part

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req,res){
-    console.log('request was made'+ req.url);
-    res.writeHead(200,{'Content-Type':'application/json'});
-    var myobj = {
-        name : "ran",
-        job : "se",
-        age : "23"
-    };
-    res.end(JSON.stringify(myobj));
+    console.log('request was made: '+ req.url);
+    if(req.url==='/home' || req.url==='/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
+    }
+    else if(req.url==='/contact'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname +'/contact.html').pipe(res);
+    }
+    else if(req.url==='/randika/json'){
+        var myObj = [{name : "randika", age:24},{name : "ramila" , age:30}];
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(myObj));
+    }else{
+        res.writeHead(404,{'Content-Type':'text.html'});
+        fs.createReadStream(__dirname+'/404.html').pipe(res);
+    }
+    
 });
     console.log('now server listning');
     server.listen('3000','127.0.0.1');
+
+    
